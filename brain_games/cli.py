f03c18002adaa44a19c7b1b268dc86e7bc5f97f0
uhbif19 <uhbif19@gmail.com>
2021-02-20 00:03:22 +0300

    Intorduce some pre-file-ignores and fix linting errors

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,9 @@
+"""Helpers for working with CLI."""
+
 import prompt
 
 
 def welcome_user():
+    """Ask user for his name in CLI and welcome him."""
     user_name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(user_name))
+    print('Hello, {0}!'.format(user_name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+"""Main script, starting the games."""
 
 from brain_games import cli
 
 
 def main():
+    """Start games in CLI."""
     print('Welcome to the Brain Games!')
     cli.welcome_user()
 
