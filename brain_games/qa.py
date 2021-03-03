97d2143592da687a78f7201f7c45ec3c653bc1b5
uhbif19 <uhbif19@gmail.com>
2021-03-03 22:23:06 +0300

    Refactor: make QA to namedtuple

--- brain_games/qa.py
+++ brain_games/qa.py
@@ -1,15 +1,7 @@
 """Module for QA dataclass."""
 
-from dataclasses import dataclass
+from collections import namedtuple
 
+# Pair of question and expected answer. To be asked to user in quiz game.
 
-@dataclass
-class QA:
-    """
-    Pair of question and expected answer.
-
-    Exists to be asked to user in quiz game.
-    """
-
-    question: str
-    correct_answer: str
+QA = namedtuple('QA', ['question', 'correct_answer'])
